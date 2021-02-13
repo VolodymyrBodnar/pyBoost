2716bb3ff9a70be76a902c87f42cf4b42bba866f
VBodnar <vbodnar72@gmail.com>
2021-02-13 21:26:05 +0200

    add methods

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+# formatting
 name = int(input('Введите имя   '))
 string = "Hello  %s" % name 
 string = "Hello  {}".format(name)
 string = f"Hello  {name}"
 
 print(string)
+
+# methods
+example = 'aaa  aAaAaAaAaA bbb ccc'
+print(example.lower())
+print(example.upper())
+print(example.capitalize())
+
+
+print(f"count of ccc in example = {example.count('ccc')}")
+print(f"first occurence of substring bbb = {example.find('bbb')}")
+
+print(f"раньше я выглядел вот так: {example}, но потом меня replace на {example.replace('aA', '1')}")
